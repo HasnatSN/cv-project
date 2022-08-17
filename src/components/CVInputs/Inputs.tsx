af237de4831ddb58cc8cda8abcc35e61fe6fe2f1
HasnatSN <hasnat.kontakt@gmail.com>
2022-08-17 22:13:36 +0200

    worked more on the design

--- src/components/CVInputs/Inputs.tsx
+++ src/components/CVInputs/Inputs.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Inputs() {
-  return <div>Bunch of Input fields</div>;
+  return (
+    <div className="button-div">
+      <button className="reset-button">Reset</button>
+      <button>+ Experience</button>
+      <button>+ Education</button>
+      <button className="pdf-button">Turn to PDF</button>
+    </div>
+  );
 }
 
 export default Inputs;
--- src/components/CVPreview/PreviewInfo.tsx
+++ src/components/CVPreview/PreviewInfo.tsx
@@ -2,15 +2,24 @@ import React from "react";
 
 function PreviewInfo() {
   return (
-    <div>
-      <h4>Decription</h4>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ea
-        veritatis id ex temporibus natus, nostrum similique libero, eos
-        repellendus ratione quos. Magnam dolorem laborum, dicta exercitationem
-        sunt dolores adipisci! Cumque iusto, unde accusamus ea placeat aliquid
-        suscipit veniam nemo deleniti harum inventore quos voluptate dolores
-        optio, obcaecati similique! Cumque.
+    <div className="preview-info">
+      <div className="description">
+        <h4>Decription</h4>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ea
+          veritatis id ex temporibus natus, nostrum similique libero, eos
+          repellendus ratione quos. Magnam dolorem laborum, dicta exercitationem
+          sunt dolores adipisci! Cumque iusto, unde accusamus ea placeat aliquid
+          suscipit veniam nemo deleniti 
+        </div>
+      </div>
+      <div className="experience">
+        <h4>Experience</h4>
+        <div>-</div>
+      </div>
+      <div className="education">
+        <h4>Education</h4>
+        <div>-</div>
       </div>
     </div>
   );
